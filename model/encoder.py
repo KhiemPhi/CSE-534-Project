ae4a259bd012df5ff96325099f3bb68e12251dc6
KhiemPhi <khiemais@gmail.com>
2021-12-01 09:49:34 -0500

    added backbone network

--- model/encoder.py
+++ model/encoder.py
@@ -1,4 +1,16 @@
+import timm
+import torch.nn as nn 
 
+
+class Encoder(nn.Module):
+
+    def __init__(self, model_name="resnet50"):
+        super().__init__()
+        self.backbone = timm.create_model(model_name, pretrained=False, num_classes=0)
+
+    def forward(self, img_batch):
+        backbone_feats = self.backbone(img_batch)
+        return backbone_feats
 
 
 
